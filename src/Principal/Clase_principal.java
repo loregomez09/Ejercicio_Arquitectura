8526c4e6744e78c20145788f8db2d5acc0b8c66d
lore09gomez <dngf07@gmail.com>
2023-02-09 20:33:35 -0500

    creacion de rama

--- src/Principal/Clase_principal.java
+++ src/Principal/Clase_principal.java
@@ -3,5 +3,6 @@ package Principal;
 public class Clase_principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("mi primera rama");
     }
 }
